178e77b0032628d433fc0f44c2794e9ec8a337b5
andrewambrosier <andrew.ambrosier@ndsu.edu>
2023-12-06 19:37:55 -0600

    added to the admin page

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,9 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CSCI213___Assignment_5.Data;
+using CSCI213___Assignment_5.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CSCI213___Assignment_5.Controllers {
     public class AdminController : Controller {
         public IActionResult Index() {
             return View();
+        }
+
+        private readonly CSCI213___Assignment_5Context dbcon;
+
+        public AdminController(CSCI213___Assignment_5Context dbcon) { 
+            this.dbcon = dbcon; 
+        }
+
+        [HttpPost]
+        public IActionResult Add(Song song) {
+            dbcon.Song.Add(song);
+            dbcon.SaveChanges();
+            return RedirectToAction("Index"); //Replace view
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Song song) {
+            dbcon.Song.Update(song);
+            dbcon.SaveChanges();
+            return RedirectToAction("Index"); //Replace view
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Song song) { 
         }
     }
 }
